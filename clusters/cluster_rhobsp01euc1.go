dbac6920c984b792b0d05f0dc81eaaca172f79d4
Moad Zardab <mzardab@redhat.com>
2026-03-04 15:48:00 +0400

    Scaling out ue1/euc1 production to allow receivers to recover from excess requests.

--- clusters/cluster_rhobsp01euc1.go
+++ clusters/cluster_rhobsp01euc1.go
@@ -69,5 +69,10 @@ func rhobsp01euc1BuildSteps() []string {
 
 // rhobsp01euc1TemplateMaps returns template mappings specific to the rhobsp01euc1 production cluster
 func rhobsp01euc1TemplateMaps() TemplateMaps {
-	return DefaultBaseTemplate().Override()
+	return DefaultBaseTemplate().Override(
+		Replicas{
+			// TODO: @moadz temporary scale out to deal with stampeding herd of retries
+			ReceiveIngestorDefault: 4,
+		},
+	)
 }
